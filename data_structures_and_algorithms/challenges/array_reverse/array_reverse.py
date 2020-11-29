33071281483b4cb3fe386972f9d675a9ffcf7385
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-11-29 23:08:53 +0200

    array_reverse

--- data_structures_and_algorithms/challenges/array_reverse/array_reverse.py
+++ data_structures_and_algorithms/challenges/array_reverse/array_reverse.py
@@ -1,8 +1,12 @@
 def reverse_array(arr):
-    """Reverses a list
-    Args:
-        arr (list): python list
-    Returns:
-        [list]: list in reversed form
-    """
+    size=len(arr)
+    index=size-1
+    for i in range(0,(size//2)):
+        temp=arr[index]
+        arr[index]=arr[i]
+        arr[i]=temp
+        (index)-=1
     return arr
+
+list=[1,2,3,4,5,6,8]
+print(reverse_array(list))
