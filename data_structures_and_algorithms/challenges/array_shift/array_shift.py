fcf417ab72f1458ae4c37641bc83e3cc113eb83c
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-11-30 23:30:32 +0200

    stretch goal

--- data_structures_and_algorithms/challenges/array_shift/array_shift.py
+++ data_structures_and_algorithms/challenges/array_shift/array_shift.py
@@ -11,6 +11,25 @@ def add_middle(arr,x):
         else:
             arr2.append(x)
     return arr2
-a=[1,2,3,4,5,6,7]
+########################################
+
+# stretch goal
+
+########################################
+
+
+def remove_middle(arr,x):
+    size=len(arr)
+    mid=size//2
+    # print(size,mid)
+    arr2=[]
+    for i in range(size):
+        if i!=mid:
+            arr2.append(arr[i])
+        else:
+            arr2.append(x)
+    return arr2
+a=[1,2,3,4,5,6]
 b=10
 print(add_middle(a,b))
+print(remove_middle(a,b))
