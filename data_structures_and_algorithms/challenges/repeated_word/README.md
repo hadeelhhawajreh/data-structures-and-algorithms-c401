e6604a3fcb3afe4a0a56b14ccdcf17a91956a7ad
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2021-01-17 09:25:04 +0200

    fixing the repeated word and adding whiteboard

---
+++ data_structures_and_algorithms/challenges/repeated_word/README.md
@@ -0,0 +1,15 @@
+# REPETED WORD
+create function called Repeat that take  a string and count each value how many repeated and return the the first repeated word 
+
+
+
+## Challenge
+<!-- Description of the challenge -->
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+space o(n)
+time o(n2)
+## Solution
+<!-- Embedded whiteboard image -->
+![img](../../assets/repeated.png)
--- data_structures_and_algorithms/challenges/repeated_word/repeated_word.py
+++ data_structures_and_algorithms/challenges/repeated_word/repeated_word.py
@@ -1,23 +1,25 @@
 from collections import Counter  
 import re
-def Repeat(input):  
-    words = input.lower().replace(',',' ').replace('.',' ').replace('?',' ').split()
-    # x=re.split('[ ? .,]',words)
-    # print(x,type(x))
-      
-    dict = Counter(words)
-    # print(dict)
-    # print('most common', Counter.most_common(dict)[0])
-    for key in words:  
-        if dict[key]>1:  
-            return (key)  
-            return
+def repeat_word(input):
+    arr = []
+    repeat = ''
+    words = re.sub(r'[^\w\s]','',input, re.UNICODE).split()
+    # print('words ---->',words)
+    #  ['It', 'was', 'a', 'queer', 'sultry', 'summer', 'the', 'summer', 
+    # 'they', 'electrocuted', 'the', 'Rosenbergs', 'and', 'I', 'didnt', 'know', 
+    # 'what', 'I', 'was', 'doing', 'in', 'New', 'York']
+    for ele in words: 
+        if ele in arr: 
+            repeat = ele
+            break
+        arr.append(ele) 
+    # print(arr)
+    return repeat
+
+
 
 def Repeat2(input):  
-    words = input.lower().replace(',',' ').replace('.',' ').replace('?',' ').split()
-    # x=re.split('[ ? .,]',words)
-    # print(x,type(x))
-      
+    words = input.lower().replace(',',' ').replace('.',' ').replace('?',' ').split()      
     dict = Counter(words)
     print(dict)
     print('most common', Counter.most_common(dict)[0])
@@ -25,11 +27,15 @@ def Repeat2(input):
         if dict[key]>1:  
             print (key, dict[key])  
             return
-# Driver program  
 if __name__ == "__main__":  
     x="Once upon a time, there was a brave princess who..."
-    Repeat(x)
+    print("🚀 ~ file: repeated_word.py ~ line 27 ~ x", repeat_word(x))
+    
     y="It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."	
-    Repeat(y)
+    print("🚀 ~ file: repeated_word.py ~ line 30 ~ x", repeat_word(y))
+
     z="It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York..."
-    Repeat(z)
+    print("🚀 ~ file: repeated_word.py ~ line 42 ~ repeat_word ", '   ', repeat_word(z))
+
+
+
